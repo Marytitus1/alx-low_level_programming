448b542acb7914ee6b9ab119a0e6a855d6ed33b5
Marytitus1 <marytitus802@gmail.com>
2022-12-12 06:06:53 -0800

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*
+ * main - a C programme using printf
+ *
+ * Return: 0
+ */
+int main(void)
+	printf("ith proper grammar, but the outcome is a piece of art,\n");
+	return (0);
