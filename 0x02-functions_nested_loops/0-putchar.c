c09b48b3da2dc8b7240ce8f3873c2e7706a7e701
Marytitus1 <marytitus802@gmail.com>
2022-12-13 08:37:41 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - prints _putchar and a new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+_putcher('_');
+_putcher('p');
+_putcher('u');
+_putcher('t');
+_putcher('c');
+_putcher('h');
+_putcher('a');
+_putcher('r');
+_putcher('\n');
+return (0);
+}
