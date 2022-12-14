b0dddd3ced828d6168ff492e7b0661a3754b104a
Marytitus1 <marytitus802@gmail.com>
2022-12-13 16:43:32 -0800

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,21 @@
 #include "main.h"
 /**
- * main - prints _putchar and a new line
+ * main - print _putchar
  *
  * Return: 0
  */
+
+
 int main(void)
 {
-_putcher('_');
-_putcher('p');
-_putcher('u');
-_putcher('t');
-_putcher('c');
-_putcher('h');
-_putcher('a');
-_putcher('r');
-_putcher('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
