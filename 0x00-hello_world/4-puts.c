ea2eeb21bf51e5dce4724c61c538bcfcb9b88bec
Marytitus1 <marytitus802@gmail.com>
2023-03-16 22:52:16 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main: printing with puts function
- * Return 0: on success
+ * main - printing with puts function
+ * Return: 0 on success
  * On error return -1
  */
 int main(void)
