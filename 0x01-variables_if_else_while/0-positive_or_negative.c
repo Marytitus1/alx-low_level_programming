25fa62fd059a90d5c98794026b81147f88b94333
Marytitus1 <marytitus802@gmail.com>
2023-03-17 09:01:13 -0700

    final

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
+
 /**
- * main - print a random number 
+ * main - print a random number
+ *
  * Return: 0 on success
- * On error: return 1
  */
 
 /* betty style doc for function main goes there */
