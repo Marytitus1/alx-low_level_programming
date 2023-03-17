ec48978aa72ba28234e9ed747fa4442d639edc8b
Marytitus1 <marytitus802@gmail.com>
2023-03-16 22:31:05 -0700

    Hello

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main: using the printf function to print to stdout
+ * Return 0: on success
+ * On error: return -1
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
