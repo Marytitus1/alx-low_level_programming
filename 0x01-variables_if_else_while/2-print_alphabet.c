bb6a955cfbe2c17202e675033f73125f76b54d83
Marytitus1 <marytitus802@gmail.com>
2023-03-17 17:49:49 -0700

    to lower

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - print alphabets in lowercase using putchar
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+int i;
+
+for (i = 'A'; i <= 'Z'; i++)
+{
+	i = tolower(i);
+	putchar(i);
+}
+return (0);
+}
