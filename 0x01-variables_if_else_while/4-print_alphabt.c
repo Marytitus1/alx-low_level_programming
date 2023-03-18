3046a7ec6e181f58e139de30c699b66e9446da2f
Marytitus1 <marytitus802@gmail.com>
2023-03-17 21:25:39 -0700

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - print alphabet in lowercase
+ * exceptions: do not print letter "q" and "e"
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	if (letter != 'q' || letter != 'e')
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
+}
