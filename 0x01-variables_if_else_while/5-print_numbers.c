3a982f62d1d2d0932c626f86c3553fa1b47a260d
Marytitus1 <marytitus802@gmail.com>
2023-03-17 22:00:12 -0700

    Numbers fixed

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 /**
  * main - program that prints single digits of base '10'
@@ -8,11 +10,11 @@
 int main(void)
 {
 
-	int i;
+	char i;
 
 	for (i = 0; i < 10; i++)
 	{
-		printf("%d", i);
+		putchar(i);
 	}
 
 	return (0);
