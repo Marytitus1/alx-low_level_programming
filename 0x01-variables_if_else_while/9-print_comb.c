524c74cd6120076b955ef05d978894e0ad335f1d
Marytitus1 <marytitus802@gmail.com>
2023-03-18 00:35:54 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints all possible combinations of single-digit numbers
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 10; i < 20; i++)
+	{
+		putchar((i % 10) + '0');
+		if (i != 19)
+		{
+			putchar(',');
+		        putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
