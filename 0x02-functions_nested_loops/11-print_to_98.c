9ae090ac2c03392423ef4923e10ef1dc1a9ee5b3
Marytitus1 <marytitus802@gmail.com>
2023-03-21 20:16:07 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,28 +10,25 @@
 
 void print_to_98(int n)
 {
-	int a, b;
-
-	for (a = n; a <= 98; a++)
-	{
-		if (a < 98)
-		{
-			printf("%d, ", n);
-		}
-		else if (a == 98)
-		{
-			printf("%d\n", n);
-		}
-		for (b = n; b > 98; b--)
-		{
-			if (b != 98)
-			{
-				printf("%d, ", n);
-			}
-			else if (b == 98)
-			{
-				printf("%d\n", n);
-			}
-		}
-	}
+int i, j;
+if (n <= 98)
+{
+for (i = n; i <= 98; i++)
+{
+if (i != 98)
+printf("%d, ", i);
+else if (i == 98)
+printf("%d\n", i);
+}
+}
+else if (n >= 98)
+{
+for (j = n; j >= 98; j--)
+{
+if (j != 98)
+printf("%d, ", j);
+else if (j == 98)
+printf("%d\n", j);
+}
+}
 }
